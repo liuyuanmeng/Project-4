56e9c91043ab8ad15fcab3e215d98c417ec37ecf
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2022-06-17 07:31:39 +0100

    delete google.js

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -8,7 +8,6 @@ import { Container, Card } from 'react-bootstrap'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Google from './Google'
 
 
 
